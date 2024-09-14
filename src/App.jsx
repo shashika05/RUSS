59301028563fd9aff791d6f843d4a560ff38b8e0
Thanuja Priyadarshane <thanujapriyadarshane@Thanujas-MacBook-Air.local>
2024-09-14 22:37:30 +0530

    Routing added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+            <p>App.jsx eka thypalla backend API tikata, Components folder eke Components tika hdannoooo, Routing dammoo, App.jsx eke Routes tika define krgnna</p>
+      </div>
+  )
+}
+
+export default App
