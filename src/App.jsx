c2503465bebf5b53ebe66882d2fa3d6ec1fc8e50
Janith Ranasinghe <janithraditha@gmail.com>
2024-09-16 00:35:24 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import './App.css'
-import Footer from "./components/footer/footer.jsx";
+import './App.css';
+import { Route, Routes} from "react-router-dom";
+import Footer from "./components/footer/Footer.jsx";
 import NavBar from "./components/navigation/NavBar.jsx";
+import Home from "./components/home/Home.jsx";
+import Symposia from "./components/symposia/Symposia.jsx";
+import Gallery from "./components/gallery/Gallery.jsx";
+import Team from "./components/team/Team.jsx";
+import Login from "./components/login/Login.jsx";
+import NotFound from "./components/notfound/NotFound.jsx";
 
 function App() {
 
-  return (
-      <div>
-          <div>
-              <NavBar/>
-          </div>
-          <div>
-              <Footer/>
-          </div>
-      </div>
-  )
+    return (
+        <div>
+            <div>
+                <NavBar/>
+            </div>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/Symposia" element={<Symposia/>}/>
+                    <Route path="/Gallery" element={<Gallery/>}/>
+                    <Route path="/Team" element={<Team/>}/>
+                    <Route path="/Login" element={<Login/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </div>
+            <div>
+                <Footer/>
+            </div>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/symposia/Symposia.jsx
@@ -0,0 +1,5 @@
+export default function Symposia(){
+    return(
+        <div>Symposia</div>
+    )
+}
