5debb5dcd713aa01d4b4691aa679add8103c631f
IsuruSH <shanakaisuru11@gmail.com>
2024-10-09 11:43:19 +0530

    Refactor ProjectCard component structure and styling

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -1,30 +1,30 @@
+/* eslint-disable react/prop-types */
 const ProjectCard = (props) => {
-    return (
-        <div className="border-2 flex flex-col items-center group rounded-xl text-center pb-7">
-            <div className="w-full">
-                <img
-                    src={props.values.img}
-                    alt=""
-                    className="rounded-t-xl object-cover"
-                />
-            </div>
-            <div className="bg-buttonbg mx-auto items-center w-[15%] h-[4px] mt-6 transition-all duration-500 ease-in-out group-hover:w-[50%] rounded-3xl"></div>
-            <h1 className="font-semibold text-xl md:text-xl mt-8 mb-5">
-                {props.values.title}
-            </h1>
-            <p className="text-textcolor mb-3">{props.values.description}</p>
+  return (
+    <div className="border-2 flex flex-col items-center group rounded-xl text-center pb-7">
+      <div className="w-full">
+        <img
+          src={props.values.img}
+          alt=""
+          className="rounded-t-xl object-cover"
+        />
+      </div>
+      <div className="bg-buttonbg mx-auto items-center w-[15%] h-[4px] mt-6 transition-all duration-500 ease-in-out group-hover:w-[50%] rounded-3xl"></div>
+      <h1 className="font-semibold text-xl md:text-xl mt-8 mb-5">
+        {props.values.title}
+      </h1>
+      <p className="text-textcolor mb-3">{props.values.description}</p>
 
+      <div className="flex-grow"></div>
 
-            <div className="flex-grow"></div>
-
-            <a
-                href={props.values.link}
-                className="text-textcolor border-b-2 border-[#6D2C95] hover:text-[#6D2C95] transition-all duration-300 ease-in-out mt-auto"
-            >
-                {props.values.linktext}
-            </a>
-        </div>
-    );
+      <a
+        href={props.values.link}
+        className="text-textcolor border-b-2 border-[#6D2C95] hover:text-[#6D2C95] transition-all duration-300 ease-in-out mt-auto"
+      >
+        {props.values.linktext}
+      </a>
+    </div>
+  );
 };
 
 export default ProjectCard;
